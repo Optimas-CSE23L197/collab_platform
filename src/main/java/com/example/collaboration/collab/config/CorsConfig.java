51b709d90989a46b430db56d0dbaedd01c893f3b
Optimas-CSE23L197 <karananimesh144@gmail.com>
2025-06-13 23:28:38 +0530

    Update CORS configuration to allow requests from production URL

--- src/main/java/com/example/collaboration/collab/config/CorsConfig.java
+++ src/main/java/com/example/collaboration/collab/config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfiguration.setAllowedOrigins(
+                List.of("http://localhost:5173", "https://collabplatform-production.up.railway.app/"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
