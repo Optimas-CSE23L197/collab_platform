8acaad55e764ac9a7677c4784a99398d79486ca1
Optimas-CSE23L197 <karananimesh144@gmail.com>
2025-06-13 23:58:01 +0530

    Refactor JwtAuthFilter to define request path variable earlier in the doFilterInternal method

--- src/main/java/com/example/collaboration/collab/filter/JwtAuthFilter.java
+++ src/main/java/com/example/collaboration/collab/filter/JwtAuthFilter.java
@@ -31,6 +31,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        String path = request.getServletPath();
+
         // ✅ Skip filter for public endpoints
         if (path.startsWith("/auth/")) {
             filterChain.doFilter(request, response);
